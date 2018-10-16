f7a9c01756cead63c3942349b80cf2f74dca92a4
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-10-16 22:10:16 +0000

    Basic server testing complete

--- test/test-server.js
+++ test/test-server.js
@@ -9,27 +9,45 @@ chai.use(chaiHttp);
 const expect = chai.expect;
 
 // Load module
-const server = require('../server');
-
+const { app, runServer, closeServer } = require('../server');
 
 // Test basic server functionality
 describe('Server start/stop functions', function() {
   
   describe('runServer()', function() {
     it('Should be a function', function() {
-      expect(server.runServer).to.be.a('function');
+      expect(runServer).to.be.a('function');
     }); 
     it('Should return a promise', function() {
-      expect(server.runServer()).to.be.a('promise');
+      expect(runServer()).to.be.a('promise');
     });
   });
   
   describe('closeServer()', function() {
     it('Should be a function', function() {
-      expect(server.closeServer).to.be.a('function');
+      expect(closeServer).to.be.a('function');
     });
     it('Should return a promise', function() {
-      expect(server.closeServer()).to.be.a('promise');
+      expect(closeServer()).to.be.a('promise');
+    });
+  });
+  
+  describe('Test static route (/)', function() {
+    
+    before(function() {
+      return runServer();
+    });
+    
+    after(function() {
+      return closeServer();
+    });
+    
+    it('Should return a 200 status code', function() {
+      return chai.request(app)
+        .get('/')
+        .then(function(res) {
+          expect(res).to.have.status(200);
+        });
     });
   });
 });
