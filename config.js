0b33275e634642ae8ce7c86b0930edf2b0b92e39
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-10-16 22:51:00 +0000

    MongoDB and mLab integration complete

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/board-match-platform';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/board-match-platform';
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+  'mongodb://a48fa83jaf:3V3DkJ6XA!xTHf@ds135003.mlab.com:35003/nonprofit-board-match';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+  'mongodb://testUser:g3ttingTesty!@ds135003.mlab.com:35003/nonprofit-board-match-dev';
 exports.PORT = process.env.PORT || 8080;
